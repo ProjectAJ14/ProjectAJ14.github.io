0c62b57e1a61c647f085a32b341be0748e97a16c
Ajay Kumar <ajay.kumar@nonstopio.com>
2022-09-20 23:50:11 +0530

    Added loader till Flutter loads up

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_site/ui/theme/theme.dart';
 
-import 'ui/theme/theme.dart';
 import 'utils/router/app_router.dart';
 
 class MyApp extends StatelessWidget {
