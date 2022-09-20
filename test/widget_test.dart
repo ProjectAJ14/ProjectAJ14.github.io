3a0f3452672001063d2ee1ae028c69954792ad60
Ajay Kumar <ajay.kumar@nonstopio.com>
2022-09-20 18:27:06 +0530

    Added simple test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_site/ui/home_screen.dart';
+
+void main() {
+  //Test Profile Image
+  testWidgets('Test Profile Image', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: HomeScreen(),
+      ),
+    );
+    // Render Image from assets
+    expect(find.byType(Image), findsOneWidget);
+  });
+}
