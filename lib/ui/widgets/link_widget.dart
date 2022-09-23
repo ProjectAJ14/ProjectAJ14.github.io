980e77b2c6225803e338e5db00f458a9bc15abb6
Ajay Kumar <ajay.kumar@nonstopio.com>
2022-09-24 02:47:28 +0530

    Added more animation

--- lib/ui/widgets/link_widget.dart
+++ lib/ui/widgets/link_widget.dart
@@ -1,4 +1,6 @@
+import 'package:animate_it/animate_it.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:my_site/data/repos/models/links.dart';
 import 'package:my_site/data/services/url_service.dart';
 
@@ -16,30 +18,33 @@ class LinkWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     String i = link.title.toLowerCase();
     String path = 'assets/images/$i.png';
-    return Center(
-      child: GestureDetector(
-        onTap: () => AppURlService.launchURL(link.url),
-        child: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Column(
-            children: [
-              Image.asset(
-                path,
-                width: size,
-                height: size,
-                filterQuality: FilterQuality.high,
-              ),
-              const SizedBox(height: 4),
-              FittedBox(
-                child: Text(
-                  link.title,
-                  style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
+    return RubberBand(
+      delay: 3000.ms,
+      child: Center(
+        child: GestureDetector(
+          onTap: () => AppURlService.launchURL(link.url),
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              children: [
+                Image.asset(
+                  path,
+                  width: size,
+                  height: size,
+                  filterQuality: FilterQuality.high,
                 ),
-              ),
-            ],
+                const SizedBox(height: 4),
+                FittedBox(
+                  child: Text(
+                    link.title,
+                    style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
