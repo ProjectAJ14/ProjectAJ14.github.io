21586d1f11eb551e459d71b4291528acacac9e5d
Ajay Kumar <ajay.kumar@nonstopio.com>
2022-09-24 01:30:08 +0530

    Updated tests

---
+++ test/data/repos/models/links_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:my_site/data/repos/models/links.dart';
+
+void main() {
+//Test if Link has image
+  test('Link has image', () {
+    final link = Link(image: 'https://i.ibb.co/ZG617z8/maxresdefault.jpg');
+    expect(link.image, 'https://i.ibb.co/ZG617z8/maxresdefault.jpg');
+  });
+}
